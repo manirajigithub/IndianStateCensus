06b4e18fd3aed3f3f8b4d0e51b445b7c53d976a8
mani1 <mani1@NANI143>
2022-08-27 20:13:10 +0530

    {Rajeswari]Improved Census Data

---
+++ IndianStateCensus/CensusAnalyser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IndianStateCensus.POCO;
+using CsvHelper;
+using IndianStateCensus.DTO;
+
+namespace IndianStateCensus
+{
+    public class CensusAnalyser
+    {
+        public enum Country
+        {
+            INDIA, US, BRAZIL
+        }
+
+        Dictionary<string, CensusDTO> dataMap;
+        public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvFilePath, string dataHeaders)
+        {
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return dataMap;
+        }
+    }
+}
